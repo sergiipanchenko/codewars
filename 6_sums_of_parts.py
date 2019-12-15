e19ffaa1b74ad7d0ec7198fbb14bed0f4e1ceb98
Jack_oS <s.panchenko@lookout.com>
2019-12-15 13:31:33 +0200

    added best practice

--- 6_sums_of_parts.py
+++ 6_sums_of_parts.py
@@ -2,13 +2,24 @@ import unittest
 
 
 # Time: 5515ms Passed: 75 Failed: 0
+# def parts_sums(ls):
+#     sums = [0]*(len(ls)+1)
+#
+#     for i in range(len(ls)-1, -1, -1):
+#         sums[i] = sums[i+1]+ls[i]
+#
+#     return sums
+
+
+# best practice
+# Time: 5035ms Passed: 75 Failed: 0
 def parts_sums(ls):
-    sums = [0]*(len(ls)+1)
+    res = [sum(ls)]
 
-    for i in range(len(ls)-1, -1, -1):
-        sums[i] = sums[i+1]+ls[i]
+    for num in ls:
+        res.append(res[-1]-num)
 
-    return sums
+    return res
 
 
 class Tests(unittest.TestCase):
